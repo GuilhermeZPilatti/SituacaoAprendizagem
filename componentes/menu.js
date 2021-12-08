d60bea5cade8c47653d3bb1b562976ac9c7e82d4
GuilhermZPilatti <zapguilherme000@gmail.com>
2021-12-07 22:04:23 -0300

    Atualização 03 SA

--- componentes/menu.js
+++ componentes/menu.js
@@ -7,17 +7,17 @@ class Menu extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
             <nav>
-            <div class="nav-wrapper">
-                <a href="/" class="brand-logo center">Logo</a>
+            <div class="nav-wrapper grey darken-4">
+                <a href="/"> <img src="/logoSA.svg" class="brand-logo center"> </a>
                 <ul id="nav-mobile" class="left hide-on-med-and-down">
                     <li><a href="/index.html">Início</a></li>
                     <li><a href="/salas/relatorio_salas.html">Salas</a></li>
-                    <li><a href="/reserva/cadastro_reserva.html">Reservar sala</a></li>
+                    <li><a href="/reserva/relatorio_reservas.html">Reservas</a></li>
                     <li><a href="/Alunos/relatorio_alunos.html">Alunos</a></li>
                     <li><a href="/Professores/relatorio_professores.html">Professores</a></li>
+                    <li><a href="/Turmas/relatorio_turmas.html">Turmas</a></li>
                 </ul>
                 <ul id="nav-mobile" class="right hide-on-med-and-down">
-                    <li><a href="/login.html">Entre</a></li>
                     <li><a href="/Alunos/cadastro_alunos.html">Cadastre-se (Alunos)</a></li>
                     <li><a href="/Professores/cadastro_professores.html">Cadastre-se (Professores)</a></li>
                 </ul>
